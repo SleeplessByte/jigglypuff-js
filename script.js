626462bc68306ff65cdc53777f14993e7fa639ab
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2016-05-01 16:00:49 +0200

    ASI and buffer

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 +function() {
-  'use strict';
+  'use strict'
 
   function nextPowerOfTwo( v ) {
-    v = v - 1;
-    v |= v >> 1;
-    v |= v >> 2;
-    v |= v >> 4;
-    v |= v >> 8;
-    v |= v >> 16;
-    return v + 1;
+    v = v - 1
+    v |= v >> 1
+    v |= v >> 2
+    v |= v >> 8
+    v |= v >> 16
+    return v + 1
   }
 
   document.addEventListener("DOMContentLoaded", function( event ) {
 
-    var audioElement = document.querySelector( 'audio' );
-    var player = Jigglypuff.createPlayer( audioElement );
+    var audioElement = document.querySelector( 'audio' )
+    var player = Jigglypuff.createPlayer( audioElement )
 
+    /**
+     * Fetch ID3 tag information for a song
+     * @param  {Song} song the song
+     */
     var fetchId3 = (function () {
-      var id3Cache = {};
+      var id3Cache = {}
 
       /**
        * Fetches the ID3 tags for a song
@@ -26,80 +29,94 @@
       return function( song ) {
 
         if ( id3Cache[ song.src ] ) {
-          updatePlayingNowFromTags( id3Cache );
-          return;
+          updatePlayingNowFromTags( id3Cache )
+          return
         }
 
         id3( song.src, function( err, tags ) {
-          id3Cache[ song.src ] = tags;
+          id3Cache[ song.src ] = tags
           if ( !err )
-            updatePlayingNowFromTags( id3Cache );
-        });
-      };
-    })();
+            updatePlayingNowFromTags( id3Cache )
+        })
+      }
+    })()
 
+    /**
+     * Update the playing now
+     * @param  {Object} id3Cache the cache
+     */
     function updatePlayingNowFromTags( id3Cache ) {
-      var song = player.currentSong;
+      var song = player.currentSong
       if ( !id3Cache[ song.src ] )
-        return;
+        return
 
-      var tags = id3Cache[ song.src ];
+      var tags = id3Cache[ song.src ]
       if ( !tags )
-        return;
+        return
 
-      console.log( "Updating song display with tags for " + song.name, tags );
+      console.log( "Updating song display with tags for " + song.name, tags )
 
-      var track = song.track;
+      var track = song.track
       if ( tags.v2 )
-        track = tags.v2.track;
+        track = tags.v2.track
       else if ( tags.v1 )
-        track = tags.v1.track;
+        track = tags.v1.track
 
-      trackDisplay.innerHTML = tags.title;
-      albumDisplay.innerHTML = tags.album;
-      artistDisplay.innerHTML = tags.artist;
+      if ( tags.title )
+        trackDisplay.innerHTML = tags.title
+      if ( tags.album )
+        albumDisplay.innerHTML = tags.album
+      if ( tags.artist )
+        artistDisplay.innerHTML = tags.artist
     }
 
     /**
-     * [drawProgress description]
-     * @return {[type]} [description]
+     * Draw the current progress for playing
      */
     function drawProgress() {
-      var percentage = audioElement.currentTime / audioElement.duration * 100
+      var percentage = player.currentTime / player.duration * 100
       if ( isNaN( percentage ) )
-        percentage = 0;
+        percentage = 0
+      progressDisplay.style = "transform: translateX( -" + ( 100 - percentage ) + "% ); background-color: " + vibrantColor
 
-      progressDisplay.style = "transform: translateX( -" + ( 100 - percentage ) + "% ); background-color: " + vibrantColor;
-      var progress = setTimeout( drawProgress, 100 );
+      if (player.buffered.length == 0 )
+        percentage = 0
+      else {
+        percentage = player.buffered[0].end / player.duration * 100
+        if ( isNaN( percentage ) )
+          percentage = 0
+      }
+      bufferedDisplay.style = "transform: translateX( -" + ( 100 - percentage ) + "% ); background-color: " + vibrantColor
+
+      setTimeout( drawProgress, 100 )
     }
-
 
     /**
      * [analyseMoment description]
      * @return {[type]} [description]
      */
     function drawVisualiser() {
-      player.analyser.getByteFrequencyData( analyserBuffer );
+      player.analyser.getByteFrequencyData( analyserBuffer )
 
-      canvasContext.clearRect(0, 0, analyserWidth, analyserHeight);
+      canvasContext.clearRect(0, 0, analyserWidth, analyserHeight)
       //analyserBufferLength is at least the width -> next power of 2
       for( var i = 0; i < analyserBufferLength; i++ ) {
-        var o = analyserBuffer[i];
-        var barHeight = (o / 255) * analyserHeight + 2;
+        var o = analyserBuffer[i]
+        var barHeight = (o / 255) * analyserHeight + 2
         if (i % analyserSkip === 0) {
-          canvasContext.beginPath();
-          canvasContext.rect( analyserPixelPerfectWidth * i, analyserHeight - barHeight, analyserPixelPerfectWidth, barHeight);
+          canvasContext.beginPath()
+          canvasContext.rect( analyserPixelPerfectWidth * i, analyserHeight - barHeight, analyserPixelPerfectWidth, barHeight)
 
-          var grad = canvasContext.createLinearGradient(0, analyserHeight,0,0);
-          grad.addColorStop(0, vibrantColor );
-          grad.addColorStop(1, vibrantColor);
+          var grad = canvasContext.createLinearGradient(0, analyserHeight,0,0)
+          grad.addColorStop(0, vibrantColor )
+          grad.addColorStop(1, vibrantColor)
 
-          canvasContext.fillStyle = grad;
-          canvasContext.fill();
+          canvasContext.fillStyle = grad
+          canvasContext.fill()
 
         }
       }
-      var analyse = requestAnimationFrame( drawVisualiser );
+      requestAnimationFrame( drawVisualiser )
     }
 
     /**
@@ -107,161 +124,156 @@
      * @param {Integer} i the album index
      */
     function playAlbum( album ) {
-      player.setPlaylist( album.songs.map( function(s) { s.album = album; return s; } ) ).next();
+      function assignAlbum( s ) {
+        s.album = album
+        return s
+      }
+      player.setPlaylist( album.songs.map( assignAlbum ) ).next()
     }
 
     /**
-     * Play
-     * @param  {[type]} event [description]
-     * @return {[type]}       [description]
+     * Handle clicking on the progress track
+     * @param  {Event} event The click event
      */
     function onProgressTrackClicked( event ) {
-      var pos = event.offsetX / progressTrack.getBoundingClientRect().width;
-      player.setPosition( player.duration * pos );
-    }
-
-    /**
-     * [vibrant description]
-     * @param  {[type]} source [description]
-     * @return {[type]}        [description]
-     */
-    function vibrant( source ) {
-      var vibrant = new Vibrant( source );
-      return vibrant.swatches();
-      /*
-       * Results into:
-       * Vibrant #7a4426
-       * Muted #7b9eae
-       * DarkVibrant #348945
-       * DarkMuted #141414
-       * LightVibrant #f3ccb4
-       */
+      var pos = event.offsetX / progressTrack.getBoundingClientRect().width
+      player.setPosition( player.duration * pos )
     }
 
     /**
      * [resize description]
      * @return {[type]} [description]
      */
-    var resizeQueued = false;
+    var resizeQueued = false, oneMoreResize = false
     function onResize() {
-      if ( resizeQueued )
-        return;
-      resizeQueued = true;
-      setTimeout( postResize, 0 );
+      if ( resizeQueued ) {
+        oneMoreResize = true
+        return
+      }
+      //resizeQueued = true
+      setTimeout( postResize, 0 )
     }
 
     function postResize() {
-      analyserDisplay.width = analyserDisplay.getBoundingClientRect().width;
-      analyserWidth = analyserDisplay.width;
-      analyserHeight = analyserDisplay.height;
-      player.analyser.fftSize = nextPowerOfTwo(analyserWidth * 2);
+      analyserDisplay.width = analyserDisplay.getBoundingClientRect().width
+      analyserWidth = analyserDisplay.width
+      analyserHeight = analyserDisplay.height
+      player.analyser.fftSize = nextPowerOfTwo(analyserWidth * 2)
 
-      analyserBufferLength = player.analyser.frequencyBinCount;
-      analyserBuffer = new Uint8Array( analyserBufferLength );
+      analyserBufferLength = player.analyser.frequencyBinCount
+      analyserBuffer = new Uint8Array( analyserBufferLength )
 
       // upper part of the graph will be mostly empty
-      analyserPixelPerfectWidth = Math.round( 1.0 / analyserBufferLength * analyserWidth );
-      analyserSkip = 2;
+      analyserPixelPerfectWidth = Math.round( 1.0 / analyserBufferLength * analyserWidth )
+      analyserSkip = 2
 
-      resizeQueued = false;
+      if ( oneMoreResize ) {
+        oneMoreResize = false
+        setTimeout( postResize, 0 )
+      } else {
+        resizeQueued = false
+      }
     }
 
     var analyserBufferLength, analyserBuffer, analyserWidth, analyserHeight,
-        analyserPixelPerfectWidth, analyserSkip;
+        analyserPixelPerfectWidth, analyserSkip
 
-    player.analyser.minDecibels = -90;
-    player.analyser.maxDecibels = -10;
-    player.analyser.smoothingTimeConstant = 0.85;
+    player.analyser.minDecibels = -90
+    player.analyser.maxDecibels = -10
+    player.analyser.smoothingTimeConstant = 0.85
 
-    var actionPlay = document.querySelector( '[data-jigglypuff="play"]');
-    var actionPause = document.querySelector( '[data-jigglypuff="pause"]');
-    var actionMute = document.querySelector( '[data-jigglypuff="mute"]');
-    var actionNext = document.querySelector( '[data-jigglypuff="next"]');
-    var actionPrev = document.querySelector( '[data-jigglypuff="prev"]');
-    var actionPlayAlbum = document.querySelector( '.album-listing .fab-action' );
+    var actionPlay = document.querySelector( '[data-jigglypuff="play"]'),
+        actionPause = document.querySelector( '[data-jigglypuff="pause"]'),
+        actionMute = document.querySelector( '[data-jigglypuff="mute"]'),
+        actionNext = document.querySelector( '[data-jigglypuff="next"]'),
+        actionPrev = document.querySelector( '[data-jigglypuff="prev"]'),
+        actionPlayAlbum = document.querySelector( '.album-listing .fab-action' )
 
-    var albumCoverDisplay = document.querySelector( '[data-jigglypuff="meta-album-cover"]');
-    var albumDisplay = document.querySelector( '[data-jigglypuff="meta-album"]' );
-    var artistDisplay = document.querySelector( '[data-jigglypuff="meta-artist"]' );
-    var trackDisplay = document.querySelector( '[data-jigglypuff="meta-track"]' );
-    var progressDisplay = document.querySelector( '[data-jigglypuff="song-progress"]');
-    var progressTrack = document.querySelector( '[data-jigglypuff="song-progress-track"]');
-    var analyserDisplay = document.querySelector( '[data-jigglypuff="analyser"]' );
-    var canvasContext = analyserDisplay.getContext( '2d' );
+    var albumCoverDisplay = document.querySelector( '[data-jigglypuff="meta-album-cover"]'),
+        albumDisplay = document.querySelector( '[data-jigglypuff="meta-album"]' ),
+        artistDisplay = document.querySelector( '[data-jigglypuff="meta-artist"]' ),
+        trackDisplay = document.querySelector( '[data-jigglypuff="meta-track"]' ),
+        progressDisplay = document.querySelector( '[data-jigglypuff="song-progress"]'),
+        bufferedDisplay = document.querySelector( '[data-jigglypuff="song-buffered"]'),
+        progressTrack = document.querySelector( '[data-jigglypuff="song-progress-track"]'),
+        analyserDisplay = document.querySelector( '[data-jigglypuff="analyser"]' )
 
-    actionPlay.addEventListener( 'click', player.play.bind( player ) );
-    actionPause.addEventListener( 'click', player.pause.bind( player ) );
-    actionNext.addEventListener( 'click', player.next.bind( player ) );
-    actionPrev.addEventListener( 'click', player.previous.bind( player ) );
-    actionMute.addEventListener( 'click', player.toggleMute.bind( player ) );
-    albumCoverDisplay.addEventListener( 'click', showCurrentAlbum );
-    progressTrack.addEventListener( 'click', onProgressTrackClicked );
-    actionPlayAlbum.addEventListener( 'click', onActionPlayAlbum );
+    var canvasContext = analyserDisplay.getContext( '2d' )
 
-    var vibrantColor = '#000000';
-    var fftColor = 'rgba( 0, 0, 0, .87 )';
+    actionPlay.addEventListener( 'click', player.play.bind( player ) )
+    actionPause.addEventListener( 'click', player.pause.bind( player ) )
+    actionNext.addEventListener( 'click', player.next.bind( player ) )
+    actionPrev.addEventListener( 'click', player.previous.bind( player ) )
+    actionMute.addEventListener( 'click', player.toggleMute.bind( player ) )
+    albumCoverDisplay.addEventListener( 'click', showCurrentAlbum )
+    progressTrack.addEventListener( 'click', onProgressTrackClicked )
+    actionPlayAlbum.addEventListener( 'click', onActionPlayAlbum )
+
+    var vibrantColor = '#000000'
+    var fftColor = 'rgba( 0, 0, 0, .87 )'
 
     player.on( 'jigglypuff:prepare', function( detail, e ) {
 
-      console.info( detail, e );
+      console.info( detail, e )
 
       if ( detail.currentSong ) {
-        var song = detail.currentSong;
+        var song = detail.currentSong
 
-        trackDisplay.innerHTML = song.name;
-        trackDisplay.setAttribute( 'data-song', song.track );
-        artistDisplay.innerHTML = song.artist;
+        trackDisplay.innerHTML = song.name
+        trackDisplay.setAttribute( 'data-song', song.track )
+        artistDisplay.innerHTML = (song.artist || song.album.artist).name
 
         if ( song.album  ) {
-          var album = detail.currentSong.album;
-          albumCoverDisplay.style = "background-color: " + album.color;
-          albumDisplay.setAttribute( 'data-album', album.id );
-          albumDisplay.innerHTML = album.name;
+          var album = detail.currentSong.album
 
-          window.AlbumListing.update( album, song );
+          albumCoverDisplay.style = "background-color: " + album.color
+          albumDisplay.setAttribute( 'data-album', album.id )
+          albumDisplay.innerHTML = album.name
 
-          var image = new Image();
+          window.AlbumListing.update( album, song )
+
+          var image = new Image()
           image.addEventListener( 'load', function() {
-            albumCoverDisplay.src = album.thumb;
-            var swatches = vibrant( image );
+            albumCoverDisplay.src = album.thumb
+            var swatches = vibrant( image )
             if (swatches.hasOwnProperty( 'Vibrant' ) && swatches['Vibrant'])
               vibrantColor = swatches['Vibrant'].getHex()
-              var rgb = swatches['Vibrant'].getRgb();
-              fftColor = 'rgba( ' + rgb[0] + ', ' + rgb[1] + ', ' + rgb[2] + ', 0.87 )';
+              var rgb = swatches['Vibrant'].getRgb()
+              fftColor = 'rgba( ' + rgb[0] + ', ' + rgb[1] + ', ' + rgb[2] + ', 0.87 )'
 
-          } );
-          image.src = album.thumb;
+          } )
+          image.src = album.thumb
         }
 
-        fetchId3( song );
+        fetchId3( song )
       }
-    });
+    })
 
     /**
      * When the player starts playing
      */
     function onPlayerPlay() {
-      console.log( "player is playing" );
-      actionPlay.style = "display: none;";
-      actionPause.style = "display: inline-block;";
+      console.debug( "player is playing" )
+      actionPlay.style = "display: none;"
+      actionPause.style = "display: inline-block;"
     }
 
     /**
      * When the player is paused
      */
     function onPlayerPause() {
-      console.log( "player is paused" );
-      actionPause.style = "display: none;";
-      actionPlay.style = "display: inline-block;";
+      console.debug( "player is paused" )
+      actionPause.style = "display: none;"
+      actionPlay.style = "display: inline-block;"
     }
 
     /**
      * Show the album that's in the current play
      */
     function showCurrentAlbum() {
-      var album = player.currentSong.album;
-      window.MediaLibrary.hide();
-      window.AlbumListing.show( album, player, skipToSongAndPlayAlbum );
+      var album = player.currentSong.album
+      window.MediaLibrary.hide()
+      window.AlbumListing.show( album, player, skipToSongAndPlayAlbum )
     }
 
     /**
@@ -271,10 +283,14 @@
      * @return {[type]}       [description]
      */
     function skipToSongAndPlayAlbum( album, song ) {
-      clearAndPlayAlbum( album );
+      clearAndPlayAlbum( album )
+      player.skipPrepare = true
       while( player.nextSong && player.currentSong.track != +song ) {
-        player.next();
+        player.next()
       }
+      player.skipPrepare = false
+      if ( player.currentSong )
+        player.play()
     }
 
     /**
@@ -282,8 +298,8 @@
      * @return {[type]} [description]
      */
     function onActionPlayAlbum() {
-      var album = this.getAttribute( 'data-album' );
-      clearAndPlayAlbum( Album( album ) );
+      var album = this.getAttribute( 'data-album' )
+      clearAndPlayAlbum( Album( album ) )
     }
 
     /**
@@ -292,35 +308,35 @@
      * @return {[type]}       [description]
      */
     function clearAndPlayAlbum( album ) {
-      player.clearHistory();
-      playAlbum( album );
+      player.clearHistory()
+      playAlbum( album )
     }
 
-    player.on( 'jigglypuff:play', onPlayerPlay );
-    player.on( 'jigglypuff:pause', onPlayerPause );
-    window.addEventListener( 'resize', onResize );
+    player.on( 'jigglypuff:play', onPlayerPlay )
+    player.on( 'jigglypuff:pause', onPlayerPause )
+    window.addEventListener( 'resize', onResize )
 
     // Setup
-    actionPlay.style = "display: inline-block;";
-    actionPause.style = "display: none";
+    actionPlay.style = "display: inline-block;"
+    actionPause.style = "display: none"
 
     setTimeout( function() {
       // Start
-      player.setVolume( 0.8 );
-      postResize();
+      player.setVolume( 0.8 )
+      postResize()
 
-      drawProgress();
-      drawVisualiser();
+      drawProgress()
+      drawVisualiser()
 
-      playAlbum( Album( 'era' ) );
+      playAlbum( Album( 'era' ) )
 
       window.MediaLibrary.glue( function( album ) {
-        window.AlbumListing.show( album, player, skipToSongAndPlayAlbum );
+        window.AlbumListing.show( album, player, skipToSongAndPlayAlbum )
       } )
-      window.MediaLibrary.show();
-    }, 0 );
+      window.MediaLibrary.show()
+    }, 0 )
 
-    window.Player = player;
-    return;
-  });
-}();
+    window.Player = player
+    return
+  })
+}()
